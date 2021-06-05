f17ba19ba1b75ecb8e7878ab271eb0646c1b6a87
mckeeeee <kosuke.04305@gmail.com>
2021-06-05 14:30:29 +0900

    AjaxAppの準備

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2021_05_25_060045) do
 
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.text "content"
+    t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
